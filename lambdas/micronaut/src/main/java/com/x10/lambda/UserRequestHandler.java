e44533a521e9554459268b120ddfa1bb3429959e
Daniel Raniz Raneland <raniz@factor10.com>
2023-06-01 11:49:16 +0200

    Preload DynamoDB classes when running with SnapStart

--- lambdas/micronaut/src/main/java/com/x10/lambda/UserRequestHandler.java
+++ lambdas/micronaut/src/main/java/com/x10/lambda/UserRequestHandler.java
@@ -8,17 +8,27 @@ import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.ApplicationContextBuilder;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.NonNull;
+import io.micronaut.crac.OrderedResource;
 import io.micronaut.function.aws.MicronautRequestHandler;
 import jakarta.inject.Inject;
+import org.crac.Context;
+import org.crac.Core;
+import org.crac.Resource;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Introspected
 public class UserRequestHandler
-        extends MicronautRequestHandler<User.CreateUser, User> {
+        extends MicronautRequestHandler<User.CreateUser, User>
+        implements Resource {
 
     @Inject
     protected UserRepository userRepository;
 
+    @Inject
+    protected DynamoDbClient dynamoDbClient;
+
     public UserRequestHandler() {
+        Core.getGlobalContext().register(this);
     }
 
     public UserRequestHandler(ApplicationContext applicationContext) {
@@ -39,5 +49,19 @@ public class UserRequestHandler
         builder.eagerInitSingletons(true);
         builder.eagerInitConfiguration(true);
         return builder;
+    }
+
+    @Override
+    public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+        try {
+            dynamoDbClient.listTables();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void afterRestore(Context<? extends Resource> context) throws Exception {
+
     }
 }
